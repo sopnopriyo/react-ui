7841068d54597e3f718ada6f6f5354f8135c354c
Shahin Alam <sopnopriyo@gmail.com>
2022-07-25 20:33:09 +0800

    Updated the web service url

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -3,7 +3,7 @@ module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://172.17.0.2:8080',
+      target: 'http://localhost:8080', // http://172.17.0.2:808
       changeOrigin: true,
     })
   );
